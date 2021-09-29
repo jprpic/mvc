a736a450d73071cc8c80a7dcb258e3e68a8d6e3d
Jakov Prpić <jakov.prpic23@gmail.com>
2021-09-29 15:06:24 +0200

    migrations

--- migrations/m0002_something.php
+++ migrations/m0002_something.php
@@ -1,11 +1,15 @@
 <?php
 
-class m0002_something{
-    public function up(){
-        echo "Applying migration";
+class m0002_add_password_column {
+    public function up()
+    {
+        $db = \app\core\Application::$app->db;
+        $db->pdo->exec("ALTER TABLE users ADD COLUMN password VARCHAR(512) NOT NULL");
     }
 
-    public function down(){
-        echo "Down migration";
+    public function down()
+    {
+        $db = \app\core\Application::$app->db;
+        $db->pdo->exec("ALTER TABLE users DROP COLUMN password VARCHAR(512) NOT NULL");
     }
 }
